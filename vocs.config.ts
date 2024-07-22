4a23c5caf371b4551db2f71ce7d41368f1cd01e2
TurboFolkBlakcMetal <mislav@polyflow.dev>
2024-07-22 21:29:41 +0200

    added opengraph preview

--- vocs.config.ts
+++ vocs.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vocs'
 export default defineConfig({
   title: 'Klaster SDK Docs',
   description: 'Build chain abstracted flows and dApps with ease through Klaster Interchain Commitments. ',
+  ogImageUrl: 'https://vocs.dev/api/og?logo=%https://content.pstmn.io/9af7177c-4ec7-40e4-855b-214ea62aaf5f/a2xhc3Rlcl9ib2plLnBuZw==&title=%Klaster&description=%Klaster',
   sidebar: [
     {
       text: 'Getting Started',
